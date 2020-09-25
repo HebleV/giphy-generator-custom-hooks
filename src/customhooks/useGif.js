4a101fd939db75d61f8e3b08121759cbd657aaeb
HebleV <visheble@yahoo.com>
2020-09-26 05:17:05 +0530

    Added a custom hook useGif

---
+++ src/customhooks/useGif.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API_KEY = process.env.API_KEY_REACT;
+
+const useGif = (tag) => {
+  const [gif, setGif] = useState("");
+
+  const fetchGif = async (tag) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const response = await axios.get(tag ? `${url}&tag=${tag}` : url);
+    const imgSrc = response.data.data.images.downsized_large.url;
+    setGif(imgSrc);
+    return response;
+  };
+
+  useEffect(() => {
+    fetchGif(tag);
+  }, [tag]);
+
+  return { gif, fetchGif };
+};
+
+export default useGif;
