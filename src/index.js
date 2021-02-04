8572c87f2a967ccad84caa3042cce3e84ec70354
HebleV <visheble@yahoo.com>
2021-02-05 00:22:07 +0530

    Testing sentry for error monitoring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+Sentry.init({
+  dsn: "https://e724767e24c0434cb84865f4d9dc6a0d@o516734.ingest.sentry.io/5623600",
+  integrations: [new Integrations.BrowserTracing()],
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
