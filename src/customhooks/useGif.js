6577a995311f2487549c29e8345eb11d75eeca12
HebleV <visheble@yahoo.com>
2021-02-05 01:04:08 +0530

    Testing sentry again

--- src/customhooks/useGif.js
+++ src/customhooks/useGif.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+const API_KEY = process.env.API_KEY_REACT;
 
 const useGif = (tag) => {
   const [gif, setGif] = useState("");
 
   const fetchGif = async (tag) => {
-    const url = `https://api.giphy.com/v2/gifs/random?api_key=g2YQPfl4oCsBYPMoaIWoVofIYOgQC43n`;
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
     const response = await axios.get(tag ? `${url}&tag=${tag}` : url);
     const imgSrc = response.data.data.images.downsized_large.url;
     setGif(imgSrc);
