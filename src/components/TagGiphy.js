480a57318a998aaa2fb34a03a2b2552a1fc6f959
HebleV <visheble@yahoo.com>
2021-02-05 00:23:11 +0530

    Added button to display error to test sentry working

--- src/components/TagGiphy.js
+++ src/components/TagGiphy.js
@@ -4,13 +4,16 @@ import useGif from "../customhooks/useGif";
 const TagGiphy = () => {
   const [tag, setTag] = useState("dogs");
   const { gif, fetchGif } = useGif(tag);
-
+  const throwError = () => {
+    throw new Error("Sample Error !!!");
+  }
   return (
     <div className="container">
       <h1>{tag} Giphy</h1>
       <img width="500" alt="taggifs" src={gif} />
       <input type="text" value={tag} onChange={(e) => setTag(e.target.value)} />
       <button onClick={() => fetchGif(tag)}>New Gif</button>
+      <button onClick={throwError}>Show Error</button>
     </div>
   );
 };
