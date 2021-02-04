320f48faf071beb128111bf7ffc32483044f886c
Vishal Heble <visheble@yahoo.com>
2021-02-05 00:47:35 +0530

    Removed unused line

--- src/customhooks/useGif.js
+++ src/customhooks/useGif.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-const API_KEY = process.env.API_KEY_REACT;
 
 const useGif = (tag) => {
   const [gif, setGif] = useState("");
