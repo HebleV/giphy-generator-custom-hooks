bd27a8ec18934de889143eafe21020c5b76c7570
HebleV <visheble@yahoo.com>
2021-02-05 13:05:37 +0530

    Added error boundary

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import RandomGiphy from './components/RandomGiphy';
 import TagGiphy from './components/TagGiphy';
 import './App.css';
-
+import ErrorBoundary from '../src/ErrorBoundary';
 function App() {
   return (
     <div className="App">
       <h1>Giphy Generator</h1>
+      <ErrorBoundary>
       <RandomGiphy />
       <TagGiphy />
+      </ErrorBoundary>
     </div>
   );
 }
--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,7 +1,33 @@
-import React from 'react'
-import * as Sentry from "@sentry/react";
-import RandomGiphy from "../src/components/RandomGiphy";
+// import React from 'react'
+// import * as Sentry from "@sentry/react";
+// import RandomGiphy from "../src/components/RandomGiphy";
 
-<Sentry.ErrorBoundary fallback={"An error has occurred"}>
-  <RandomGiphy />
-</Sentry.ErrorBoundary>;
+// <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+//   <RandomGiphy />
+// </Sentry.ErrorBoundary>;
+
+import React from "react";
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    console.log(error)
+    console.info(error)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Oops! seems some error occured</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
